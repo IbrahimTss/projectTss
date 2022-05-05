835c94c069bf6e48fb54435b5bbf8e9954b07a44
aasimtss <aasim@tecnovators.in>
2022-05-05 18:19:41 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './dash-board/navbar/navbar.component';
+import { UserDetailComponent } from './dash-board/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
