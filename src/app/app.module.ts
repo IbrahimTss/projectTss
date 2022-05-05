e86ff4a476fdf9c6521b7697b9e05fbecd491372
IbrahimTss <99019500+IbrahimTss@users.noreply.github.com>
2022-05-05 16:02:29 +0530

    users folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './user/signup/signup/signup.component';
+import { LoginComponent } from './user/signup/login/login/login.component';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
